94ca38f54bb2b61fc5a4f788666c5c2d3e1a4504
MateoTarifa <mtarifa17@gmail.com>
2024-10-23 19:16:51 -0300

    Update.Cambios
    Se realizan cambios respecto al diseño de las tarjetas de presentacion, se juega un poco con los colores y se intenta lograr un aspecto agradable, se busca comprender como cambiar el diseño correctamente.

--- ProyectoFinal/src/App.jsx
+++ ProyectoFinal/src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
-import './App.css'
+import './App.css'; 
 import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
 
-  return (
+  return ( 
       <div className='container' style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+
+        {/* Primera Tarjeta */}
+        <Card className = "custom-card" style={{ width: '30rem', height: '14rem', overflow: 'hidden'}}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Card.Img variant="left" src="/Cat.jpeg"  style={{ width: '150px', height: '150px', objectFit: 'cover', marginRight: '2px' }}/>
+          <Card.Body>
+            <Card.Title>Mateo Tarifa "Spine"</Card.Title>
+            <Card.Text style={{fontSize: '0.8rem', overflow: 'hidden', maxHeight: '100px'}}> 
+              Soy un aspirador a programador Front-end y desarrollador Web, me encanta la musica y los videojuegos. Soy fanatico de Gustavo Cerati y mi juego Favorito es Terraria.
+              Preferiblemente en el mundo de la programacion o de los videojuegos soy conocido como Spinerati, nombre proveniente de dos musico que admiro muchisimo, Spinetta y Cerati.
+              Mi sueño es hacer un juego que trate los temas delicados de la salud mental, la depresión y las etapas del duelo.
+            </Card.Text>
+          </Card.Body>
+        </div>
+        </Card>
+      
         <Card style={{ width: '30rem', height: '14rem', overflow: 'hidden'}}>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <Card.Img variant="left" src="/Cat.jpeg"  style={{ width: '150px', height: '150px', objectFit: 'cover', marginRight: '10px' }}/>
+          <Card.Img variant="left" src="/cassidy1.jpg"  style={{ width: '150px', height: '150px', objectFit: 'cover', marginRight: '2px' }}/>
           <Card.Body>
             <Card.Title>Mateo Tarifa "Spine"</Card.Title>
             <Card.Text style={{fontSize: '0.8rem', overflow: 'hidden', textOverflow: 'ellipsis'}}>
@@ -20,7 +37,20 @@ function App() {
           </Card.Body>
         </div>
         </Card>
-      
+
+        <Card style={{ width: '30rem', height: '14rem', overflow: 'hidden'}}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Card.Img variant="left" src="/Quito.png"  style={{ width: '150px', height: '150px', objectFit: 'cover', marginRight: '10px' }}/>
+          <Card.Body>
+            <Card.Title>Mateo Tarifa "Spine"</Card.Title>
+            <Card.Text style={{fontSize: '0.8rem', overflow: 'hidden', textOverflow: 'ellipsis'}}>
+              Soy un aspirador a programador Front-end y desarrollador Web, me encanta la musica y los videojuegos. Soy fanatico de Gustavo Cerati y mi juego Favorito es Terraria.
+              Preferiblemente en el mundo de la programacion o de los videojuegos soy conocido como Spinerati, nombre proveniente de dos musico que admiro muchisimo, Spinetta y Cerati.
+              Mi sueño es hacer un juego que trate los temas delicados de la salud mental, la depresión y las etapas del duelo.
+            </Card.Text>
+          </Card.Body>
+        </div>
+        </Card>
 
         <Card style={{ width: '30rem', height: '14rem', overflow: 'hidden'}}>
         <div style={{ display: 'flex', alignItems: 'center' }}>
