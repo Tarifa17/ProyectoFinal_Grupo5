854cb799b58a161187e97dea9b396ec98225e3b0
Esteban Ibañez <69483001+EstebanIbz@users.noreply.github.com>
2024-11-05 23:16:31 -0300

    CSS update parte 3
    --INFO--
    
    En esta actualizacion añadimos estilos a variedad de cosas.
    
    -Se actualizo el contenido dentro del desarrolladores.json, de paso agregando su viejo estilo al actual
    
    -experimentando recien con el Home.css proximas mejoras entrantes
    
    -Se agregaron imagenes al carrucel de forma de caratula y manipulado su espacio con css
    
    -correccion de espacios que restan estetica al usuario
    
    -El Nav esta terminado parcialmente hasta que pueda corregir la paleta de color y su vista
    
    -Experimentando con el Footer+ tratando de agregar iconos y una columna

--- ProyectoFinal/src/components/AboutUs.jsx
+++ ProyectoFinal/src/components/AboutUs.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
-import './AboutUs.css'; 
 import Card from 'react-bootstrap/Card';
+import './AboutUs.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import desarrolladores from "../data/desarrolladores.json"; 
+import { CardImg } from 'react-bootstrap';
 
-class AboutUs extends React.Component {
-    render() {
-        return (
-            <div className='container' style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-                {desarrolladores.map(desarrollador => (
-                    <Card key={desarrollador.id} style={{ width: '30rem', height: '14rem', overflow: 'hidden' }}>
-                        <div style={{ display: 'flex', alignItems: 'center' }}>
-                            <Card.Img variant="left" src={desarrollador.image} style={{ width: '150px', height: '150px', objectFit: 'cover', marginRight: '10px' }} />
-                            <Card.Body>
-                                <Card.Title>{desarrollador.name}</Card.Title>
-                                <Card.Text style={{ fontSize: '0.8rem', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                                    {desarrollador.description}  
-                                </Card.Text>
-                            </Card.Body>
-                        </div>
-                    </Card>
-                ))}
-
-            </div>
-        );
-    }
+function AboutUs() {
+    return (
+        <div className='container' style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+            {desarrolladores.map(desarrollador => (
+                <Card className='Card-type' key={desarrollador.id} style={{ width: '30rem', height: '12rem', overflow: 'hidden' }}>
+                    <div style={{ display: 'flex', alignItems: 'center', height: '100%'}}>
+                        <Card.Img className='ilustracion' variant="left" src={desarrollador.image} style={{ width: '150px', height: '150px', objectFit: 'cover', marginRight: '15px', marginLeft: '5px'}} />
+                        <Card.Body>
+                            <Card.Title>{desarrollador.name}</Card.Title>
+                            <Card.Text style={{ fontSize: '0.8rem', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                {desarrollador.description} 
+                                <br />                            
+                                <a                                 
+                                    href={desarrollador.github} 
+                                    target="_blank" 
+                                    rel="noopener noreferrer"
+                                    style={{ color: 'blue', textDecoration: 'underline' }}                                   
+                                >
+                                    Perfil de GitHub
+                                </a>
+                            </Card.Text>
+                        </Card.Body>
+                    </div>
+                </Card>
+            ))}
+        </div>
+    );
 }
 
 export default AboutUs;
--- ProyectoFinal/src/components/Layout.jsx
+++ ProyectoFinal/src/components/Layout.jsx
@@ -9,20 +9,17 @@ function Layout() {
         <header>
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="/">Mi proyecto Final</Navbar.Brand>
+                <Navbar.Brand className='Iconex' href="/">Mi proyecto Final</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href= "/">Home</Nav.Link>
                         <Nav.Link href="/aboutUs">About Us</Nav.Link>
-                        <NavDropdown title="Proyectos" id="basic-nav-dropdown">
+                        <NavDropdown  title="Proyectos" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/juegoPhaser">Juego Phaser</NavDropdown.Item>
-                            <NavDropdown.Item href="/gestorBillerera">
-                               Gestor de Billeteras
-                            </NavDropdown.Item>
+                            <NavDropdown.Item href="/gestorBillerera"> Gestor de Billeteras</NavDropdown.Item>                                                     
                             <NavDropdown.Item href="/juego">Desafio Matematico</NavDropdown.Item>
-                            <NavDropdown.Item href="/calcularIMC">Calculadora IMC</NavDropdown.Item>
-                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/calcularIMC">Calculadora IMC</NavDropdown.Item>                            
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
@@ -34,12 +31,12 @@ function Layout() {
 </main>
         <footer>
             <div className='footer'>
-            <p> &copy; 2024 Derechos Reservados para el GRUPO 5 FPW2024 </p>
-            <ul class="social-media">
-                <li><a href="#">Facebook</a></li>
-                <li><a href="#">Twitter</a></li>
-                <li><a href="#">Instagram</a></li>             
-            </ul>
+            <p> &copy; 2024 Derechos Reservados para el GRUPO 5 FPW2024 </p>    
+            <div href="" className='social-media'> 
+            <a href="#">Facebook</a>   
+            <a href="#">Instagram</a> 
+            <a href="#">GitHub</a> 
+            </div>             
             </div>
         </footer>
         </div>
